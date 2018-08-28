3b5970e0e342f192a39e5453b68db361f4573f43
Tatiana <tatiana.dracheva@gmail.com>
2018-08-29 01:24:15 +0300

    add birthday cake candles. The problem has not been solved yet

---
+++ birthday-cake-candles.js
@@ -0,0 +1,53 @@
+//task: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the birthdayCakeCandles function below.
+function birthdayCakeCandles(ar) {
+    var max=Number.POSITIVE_INFINITY;
+    //var n = Number.POSITIVE_INFINITY;
+    max=ar[0];
+    var n=0;
+ for (var i=0; i<ar.length; i++)
+  if(max<=ar[i])
+  {max=ar[i];
+  n=n+1;}
+console.log (n)  }
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = birthdayCakeCandles(ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
